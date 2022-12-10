e466cd38a8de952b487219193020e7da71689c93
Dale Pienaar <120015268+dalep-testing-di@users.noreply.github.com>
2022-12-11 00:53:10 +0200

    Update streamlit_app.py
    moved "the user entered" section

--- streamlit_app.py
+++ streamlit_app.py
@@ -39,12 +39,11 @@ try:
       streamlit.error("Please select a fruit to get information.")
   else:
       back_from_function = get_fruityvice_data(fruit_choice)
+      streamlit.write('The user entered ', '"' + fruit_choice + '"')
       streamlit.dataframe(back_from_function)
       
 except URLError as e:
   streamlit.error()
-  
-streamlit.write('The user entered ', '"' + fruit_choice + '"')
 
 #import requests
 #fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
