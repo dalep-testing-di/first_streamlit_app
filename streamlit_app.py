eeecff39c8b67d6947901b5728ab74f39a306885
Dale Pienaar <120015268+dalep-testing-di@users.noreply.github.com>
2022-12-11 00:38:26 +0200

    Update streamlit_app.py
    added header and changed button name

--- streamlit_app.py
+++ streamlit_app.py
@@ -58,16 +58,20 @@ streamlit.write('The user entered ', '"' + fruit_choice + '"')
 #streamlit.stop()
 
 #import snowflake.connector
-streamlit.header("The fruit load list contains:")
+streamlit.header("The fruit list contains:")
 #Snowflake-related functions
 def get_fruit_load_list():
     with my_cnx.cursor() as my_cur:
       my_cur.execute("SELECT * FROM fruit_load_list")
       return my_cur.fetchall()
+    
+streamlit.header("View Our Fruit List - Add your Favorites!")
   
 #Add a button to load the fruit
-if streamlit.button('Get Fruit Load List'): 
+if streamlit.button('Get Fruit List'): 
+  my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
   my_data_rows = get_fruit_load_list()
+  my_cnx.close()
   streamlit.dataframe(my_data_rows)
   
 #streamlit.stop()
